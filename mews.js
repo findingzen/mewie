54d02b19800b5e6293bf3cf10748bee10f02ce2b
maximilian garcia <max.garcia@inflectionit.com>
2022-03-06 15:01:56 -0700

    added randomized vimeo links

--- mews.js
+++ mews.js
@@ -18,9 +18,6 @@ function rainText() {
 
 }
 
-
-
-
 function dropPhrase(){
     //preventDefault();
 
@@ -114,15 +111,66 @@ var quotes = ["I have a lot I want to say on this topic but I need to go to bed!
   
 // var quotes = ["one", "two", "three"]; 
 var randomNumber = Math.floor(Math.random()*quotes.length);
- var text = quotes[randomNumber];
+var text = quotes[randomNumber];
     
-    var elem = document.createElement("p");
-    elem.textContent = text;
-    elem.style.position = "absolute";
-    elem.style.left = Math.round(Math.random() * fullWidth) + "px";
-    elem.style.top = Math.round(Math.random() * fullHeight) + "px";
-    document.body.appendChild(elem);
+var elem = document.createElement("p");
+elem.textContent = text;
+elem.style.position = "absolute";
+elem.style.left = Math.round(Math.random() * fullWidth) + "px";
+elem.style.top = Math.round(Math.random() * fullHeight) + "px";
+document.body.appendChild(elem);
 
 }
+  
+dropLinks();
 
 
+/* 
+<a class="fade-in-link" href="vimeo.html?video=jamais">jamais *</a><br>
+<a class="fade-in-link" href="vimeo.html?video=droid">this is droid. :: representation</a><br>
+<a class="fade-in-link" href="vimeo.html?video=coup">* Un Coup de Dés</a><br>
+<a class="fade-in-link" href="vimeo.html?video=vegetale">maison végétale</a><br>
+<a class="fade-in-link" href="vimeo.html?video=rosenkranz">Rosenkranz</a><br>
+<a class="fade-in-link" href="vimeo.html?video=tgsgtwsd">thank god she got there when she did</a><br>
+<a class="fade-in-link" href="vimeo.html?video=goals">Goals for Tue 5/5 day after I was tr…</a><br>
+*/
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+async function dropLinks() {
+    await sleep(22000);
+
+    var links =    ['jamais|jamais *',
+                    'droid|this is droid. :: representation',
+                    'coup|* Un Coup de Dés',
+                    'vegetale|maison végétale',
+                    'rosenkranz|Rosenkranz',
+                    'goals|Goals for Tue 5/5 day after I was tr…'
+                    ];
+
+
+    for (var i = 0; i < links.length; i++) {
+        const linkPart = links[i].split('|');
+        let linkParam = linkPart[0];
+        let linkTitle = linkPart[1]; 
+        var a = document.createElement('a');
+        a.className = 'fade-in-link' // or else it will use the base style for anchor tags
+        a.href = 'vimeo.html?video=' + linkParam;
+        a.title = linkTitle;
+        var linkText = document.createTextNode(linkTitle);
+        a.appendChild(linkText);
+        
+        //var text = links[i];
+       
+        a.style.position = "absolute";
+        a.style.left = Math.round(Math.random() * fullWidth) + "px";
+        a.style.top = Math.round(Math.random() * fullHeight) + "px";
+        document.body.appendChild(a);
+     
+
+    }            
+
+  
+  }
